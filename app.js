73ee55bf01fe34ff0ae7ba7469e80a3d1647381b
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-04-08 09:46:03 -0400

    update

--- app.js
+++ app.js
@@ -1,40 +1,4 @@
-// require('dotenv').config();
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const authRoutes = require('./routes/authRoutes');
-// const router = express.Router();
-// const productRoutes = require('./routes/productRoutes');
 
-// const Product = require('./models/productModel'); 
-
-// const app = express();
-
-// app.use(express.static('public'));
-
-// app.set('view engine', 'ejs');  I don't think I need this one
-
-
-// console.log('🔍 MONGO_URI =', JSON.stringify(process.env.MONGO_URI));
-//database connection
-// const dbURI = process.env.MONGO_URI; 
-// console.log('RAW URI:', [...process.env.MONGO_URI]);
-// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then((result) => app.listen(3111))
-//   .catch((err) => console.log(err));
-
-
-
-  //routes
-// app.get('/', (req, res) => res.render('home'));
-// app.get('/items', (req, res) => res.render('items'));
-
-// app.use(productRoutes);
-
-// const PORT = process.env.PORT || 3111;
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
